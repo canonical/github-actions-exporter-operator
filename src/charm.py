3d2b48b4538b5ee060fd97d44ab38d1aee6a3510
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2023-05-17 10:26:17 -0300

    fix(ISD-509): Change config.yaml and add comment to traefik service

--- src/charm.py
+++ src/charm.py
@@ -42,6 +42,8 @@ class GithubActionsExporterOperatorCharm(CharmBase):
             # We're forced to use the app's service endpoint
             # as the ingress per app interface currently always routes to the leader.
             # https://github.com/canonical/traefik-k8s-operator/issues/159
+            # For juju >= 3.1.1, this could be used in combination with open-port for true load
+            # balancing.
             host=f"{self.app.name}-endpoints.{self.model.name}.svc.cluster.local",
             strip_prefix=True,
         )
