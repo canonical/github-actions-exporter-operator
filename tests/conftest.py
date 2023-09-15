1ad4fcb08d9b1e1431daa774c4cd927022e19b96
Amanda H. L. de Andrade Katz <amanda.katz@canonical.com>
2023-09-15 11:49:27 -0300

    Fix prometheus_scrape integration by adding prometheus_alert_rules (#8)

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,4 @@
 def pytest_addoption(parser):
     """Parse additional pytest options."""
     parser.addoption("--github-actions-exporter-image", action="store")
+    parser.addoption("--charm-file", action="store", help="Charm file to be deployed")
